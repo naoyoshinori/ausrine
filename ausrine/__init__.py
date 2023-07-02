2e5d518841d9ace5cf717c88ca10e373a11eb1f4
Naoyuki Yoshinori <1448309+naoyoshinori@users.noreply.github.com>
2023-07-02 09:21:22 +0900

    Fixed to clear the textbox before entering text.

--- ausrine/__init__.py
+++ ausrine/__init__.py
@@ -2,10 +2,80 @@ import time
 
 from logging import getLogger
 
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 
 logger = getLogger(__name__)
+
+special_keys_codes = [
+    Keys.NULL,
+    Keys.CANCEL,
+    Keys.HELP,
+    Keys.BACKSPACE,
+    Keys.BACK_SPACE,
+    Keys.TAB,
+    Keys.CLEAR,
+    Keys.RETURN,
+    Keys.ENTER,
+    Keys.SHIFT,
+    Keys.LEFT_SHIFT,
+    Keys.CONTROL,
+    Keys.LEFT_CONTROL,
+    Keys.ALT,
+    Keys.LEFT_ALT,
+    Keys.PAUSE,
+    Keys.ESCAPE,
+    Keys.SPACE,
+    Keys.PAGE_UP,
+    Keys.PAGE_DOWN,
+    Keys.END,
+    Keys.HOME,
+    Keys.LEFT,
+    Keys.ARROW_LEFT,
+    Keys.UP,
+    Keys.ARROW_UP,
+    Keys.RIGHT,
+    Keys.ARROW_RIGHT,
+    Keys.DOWN,
+    Keys.ARROW_DOWN,
+    Keys.INSERT,
+    Keys.DELETE,
+    Keys.SEMICOLON,
+    Keys.EQUALS,
+    Keys.NUMPAD0,
+    Keys.NUMPAD1,
+    Keys.NUMPAD2,
+    Keys.NUMPAD3,
+    Keys.NUMPAD4,
+    Keys.NUMPAD5,
+    Keys.NUMPAD6,
+    Keys.NUMPAD7,
+    Keys.NUMPAD8,
+    Keys.NUMPAD9,
+    Keys.MULTIPLY,
+    Keys.ADD,
+    Keys.SEPARATOR,
+    Keys.SUBTRACT,
+    Keys.DECIMAL,
+    Keys.DIVIDE,
+    Keys.F1,
+    Keys.F2,
+    Keys.F3,
+    Keys.F4,
+    Keys.F5,
+    Keys.F6,
+    Keys.F7,
+    Keys.F8,
+    Keys.F9,
+    Keys.F10,
+    Keys.F11,
+    Keys.F12,
+    Keys.META,
+    Keys.COMMAND,
+    Keys.ZENKAKU_HANKAKU,
+]
+
 
 def _url_remove_suffix(url: str) -> str:
     tail_index = len(url) - 1
@@ -132,7 +202,12 @@ class Ausrine:
             e.clear()
 
     def send_keys(
-        self, by: str, value: str, text: str, time_to_wait: float = 10.0
+        self,
+        by: str,
+        value: str,
+        text: str,
+        append: bool = False,
+        time_to_wait: float = 10.0,
     ) -> None:
         """Find an element given a By strategy and locator.
         Then Simulates typing into the element.
@@ -141,6 +216,7 @@ class Ausrine:
             by (str): By strategy.
             value (str): Locator.
             text (str): A string for typing, or setting form fields.
+            append (bool, optional): If set to True, text is appended to the textbox.
             time_to_wait (float, optional): Amount of time to wait (in seconds).
             Defaults to 10.0.
         """
@@ -148,7 +224,12 @@ class Ausrine:
         if e:
             html = e.get_attribute("outerHTML")
             logger.debug("send_keys - %s", html)
-            e.send_keys(text)
+
+            if (append) or (text in special_keys_codes):
+                e.send_keys(text)
+            else:
+                e.clear()
+                e.send_keys(text)
 
     def execute(self, sequences: list[dict]):
         """Execute the commands in sequences.
@@ -188,6 +269,12 @@ class Ausrine:
                     case "clear":
                         self.clear(by=v["by"], value=v["value"])
                     case "send_keys":
-                        self.send_keys(by=v["by"], value=v["value"], text=v["text"])
+                        if "append" in v.keys():
+                            append = v["append"]
+                        else:
+                            append = False
+                        self.send_keys(
+                            by=v["by"], value=v["value"], text=v["text"], append=append
+                        )
                     case _:
                         logger.error("No method named '%s'.", k)
